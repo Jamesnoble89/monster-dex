d4b0d78e9eb847489a8c6fa9729628f6a9120d98
James Noble <jnoble@sparkenergy.local>
2020-06-17 09:38:05 +0100

    Updates

---
+++ precache-manifest.570b152e7b13889f99da55c6d67c11b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e200104d530d31e99b863043678ffb",
+    "url": "/monster-dex/index.html"
+  },
+  {
+    "revision": "0bbe5243ad5e65cf74de",
+    "url": "/monster-dex/static/css/main.e2904332.chunk.css"
+  },
+  {
+    "revision": "b506f53edad20e03c508",
+    "url": "/monster-dex/static/js/2.c57f6b69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-dex/static/js/2.c57f6b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbe5243ad5e65cf74de",
+    "url": "/monster-dex/static/js/main.915993a4.chunk.js"
+  },
+  {
+    "revision": "b1189f0fb097b486d36c",
+    "url": "/monster-dex/static/js/runtime-main.7216e670.js"
+  }
+]);
